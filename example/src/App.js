539f1fade4ecf54cb72a76fb39c7ca3aa7909403
Abhishek Tanwar <abhishek.kumartanwar@gmail.com>
2018-08-04 12:50:51 +0800

    initial

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,48 @@
 import React, { Component } from 'react'
 
-import ExampleComponent from 'rect-ui-dropdown'
+import RectUIDropdown from 'rect-ui-dropdown'
 
 export default class App extends Component {
+  onSelect = ({key, value}) => {
+    console.log('selected values:', key, value);
+  }
   render () {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <h5>Default Dropdown</h5>
+        <RectUIDropdown
+          prompt='Select Currency'
+          values={[
+            {key:'1', value: 'USD'},
+            {key:'2', value: 'MYR'},
+          ]}
+          onSelect={this.onSelect}
+        />
+
+        <h5>Searchable Dropdown</h5>
+        <RectUIDropdown
+          prompt='Select Currency'
+          values={[
+            {key:'1', value: 'USD'},
+            {key:'2', value: 'MYR'},
+          ]}
+          onSelect={this.onSelect}
+        />
+
+        <h5>Searchable Dropdown (Huge Data)</h5>
+        <RectUIDropdown
+          prompt='Select Currency'
+          values={[
+            {key:'1', value: 'USD'},
+            {key:'2', value: 'MYR'},
+          ]}
+          onSelect={this.onSelect}
+          maxSearch={10}
+        />
+
       </div>
+
+      
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -4,18 +4,68 @@ import PropTypes from 'prop-types'
 import styles from './styles.css'
 
 export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      open: false,
+    }
+    this.myRef = React.createRef();
+    this.handleOutsideClick = this.handleOutsideClick.bind(this);
+  }
+
+  toggle = () => {
+    const {open} = this.state;
+    if (!open) {
+      document.addEventListener('click', this.handleOutsideClick, false);
+    } else {
+      document.removeEventListener('click', this.handleOutsideClick, false);
+    }
+    this.setState({open: !open});
+  }
+  handleOutsideClick(e) {
+    if (this.myRef.current.contains(e.target)) {
+      return;
+    }
+    this.toggle();    
+  }
+  select = (option) => {
+    this.setState({selectedOption: option}, ()=> {
+      this.toggle(); 
+      this.props.onSelect(option)
+    });
+  }
+  renderOptions = () => {
+    const { values } = this.props;
+    const { selectedOption } = this.state;
+    return values.map((item) =>
+      <li key={item.key}
+        className={(selectedOption && selectedOption.key===item.key)?styles.selectedli:styles.defaultli} 
+        onClick={()=>this.select(item)}>
+          {item.value}
+      </li>
+    )
   }
 
   render() {
-    const {
-      text
-    } = this.props
+    const {prompt} = this.props;
+    const {open, selectedOption} = this.state;
 
     return (
-      <div className={styles.test}>
-        Example Component: {text}
+      <div className={styles.wrapper} ref={this.myRef}>
+        <div className={styles.select} onClick={this.toggle}>
+          {
+            selectedOption?<span>{selectedOption.value}</span>:<span className={styles.prompt}>{prompt}</span>
+          }
+          <span className={styles.dropDownIcon}>&#9660;</span>
+        </div>
+        <div className={open?styles.contentOpen:styles.contentClosed} onBlur={()=>console.log('out')}>
+          <ul>
+          {
+            this.renderOptions()
+          }
+          </ul>
+        </div>
       </div>
     )
   }
